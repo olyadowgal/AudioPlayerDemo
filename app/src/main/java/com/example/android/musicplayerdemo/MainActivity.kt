9bcbe33e41d78a322b596a1851eb232394f0c83a
DowgalOlya <dowgalolya@gmail.com>
2019-02-06 11:44:05 +0200

    Initial Commit

--- app/src/main/java/com/example/android/musicplayerdemo/MainActivity.kt
+++ app/src/main/java/com/example/android/musicplayerdemo/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.android.musicplayerdemo
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.google.android.exoplayer2.ExoPlayerFactory
-import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
+import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -12,9 +14,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val player = ExoPlayerFactory.newSimpleInstance(this)
-        player_view.player = player
-
     }
 }
